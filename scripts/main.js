9c8ef212aa49a8a265354835c4e7e1e36cbe0318
RayhanSohel <arayhansohel@gmail.com>
2024-10-06 21:45:52 +0600

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// ------Custom Tailwind CSS------//
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          brandColor: '#9002F1',
+        },
+        fontFamily: {
+          lato: ['Lato', 'sans-serif'],
+        }
+      }
+    }
+  }
+
+
