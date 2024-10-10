a83b5ecb8f0507e3413f6718cb01274099241369
RayhanSohel <arayhansohel@gmail.com>
2024-10-11 00:58:39 +0600

    Modal Dynamic Data Fetching

--- scripts/main.js
+++ scripts/main.js
@@ -39,7 +39,6 @@ const fetchAllPets = async (category) => {
     }
   }
 };
-
 // ------ Display Post ------//
 const displayAllPets = (data) => {
     const petContainer = document.getElementById("pet-container");
@@ -95,20 +94,19 @@ const displayAllPets = (data) => {
                 </li>
             </ul>
         </div>
-
         <div class="flex justify-between pt-4 border-t">
             <button onclick=fetchImage('${pet.image}') class="py-2 px-4 border-2 hover:border-brandColor text-brandColor hover:bg-brandColor hover:text-white rounded-xl transition-colors duration-500"><i class="fa-regular fa-thumbs-up"></i></button>
-
-            <button onclick="showAdoptModal()" class="py-2 px-6 border-2 hover:border-brandColor text-brandColor hover:bg-brandColor hover:text-white rounded-xl transition-colors duration-500">Adopt</button>
-
-            <button onclick=showModal() class="py-2 px-6 border-2 hover:border-brandColor text-brandColor hover:bg-brandColor hover:text-white rounded-xl transition-colors duration-500">Detail</button>
+            <button onclick=showAdoptModal() class="py-2 px-6 border-2 hover:border-brandColor text-brandColor hover:bg-brandColor hover:text-white rounded-xl transition-colors duration-500">Adopt</button>
+            <button onclick="showModal('${pet.image}', '${pet.pet_name.replace(/'/g, "\\'")}', '${pet.breed}', '${pet.date_of_birth}', '${pet.gender}', '${pet.price}', '${pet.vaccinated_status}', '${pet.pet_details.replace(/'/g, "\\'")}')" class="py-2 px-6 border-2 hover:border-brandColor text-brandColor hover:bg-brandColor hover:text-white rounded-xl transition-colors duration-500">Detail</button>
         </div>
       </div>
-     `
+     `;
     petContainer.appendChild(div);
   })
 }
 fetchAllPets();
+
+
 
 
 // ------ fetch all Categories ------//
@@ -131,13 +129,11 @@ const displayAllCategories = (categories) => {
       </div> 
     </button>
     </div>
-     `
+     `;
     cateContainer.appendChild(div);
   })
 }
 fetchAllCategories();
-
-
 // ------Switching Categories Menu with active status-----//
 const setActive = (id) => {
   document.querySelectorAll('.category-btn').forEach(btn => {
@@ -147,37 +143,97 @@ const setActive = (id) => {
 };
 
 
+
+
 // ------thumbnail fetch-----//
 const fetchImage = (image) => {
-  console.log(image);
   const thumbDiv = document.getElementById('thumb-container'); 
   const div= document.createElement("div");
   div.innerHTML = `
             <div>
                 <img class="sm:max-h-24 md:max-h-48 xl:max-h-24 w-full object-cover rounded-xl" src="${image}" alt="">
             </div>
-  `
+  `;
   thumbDiv.appendChild(div);
 }
 
 
 
-//-------Show the modal with window-------//
-function showModal(message) {
 
+//-------Show the modal with window-------//
+const showModal = (image, pet_name, breed, date_of_birth, gender, price, vaccinated_status, pet_details ) => {
+  console.log({ image, pet_name, breed, date_of_birth, gender, price, vaccinated_status, pet_details });
   const modal = document.getElementById('confirmationModal');
-  const modalMessage = document.getElementById('modalMessage');
-  modalMessage.innerText = message;
+  modal.innerHTML = "";
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <div class="bg-white rounded-2xl shadow-lg p-6 max-w-[600px] w-full">
+      <div>
+          <img class="md:h-72 w-full object-cover rounded-xl" src="${image}" alt="">
+      </div>
+
+      <div> 
+          <h2 class="text-2xl font-extrabold my-3">${pet_name}</h2>
+      </div>
+
+      <div class="mb-4 p-4 border-b-2">
+          <ul class="grid grid-cols-1 md:grid-cols-2 gap-1">
+              <li>
+              <i class="text-xl mr-3 text-brandColor fa-solid fa-sun"></i> 
+              <span class="font-bold">Breed: </span>
+              ${(breed === "null" || breed === "undefined" || breed === "") 
+                  ? "Not available" 
+                  : breed}
+              </li>
+              <li>
+              <i class="text-xl mr-3 text-brandColor fa-solid fa-calendar-days"></i> 
+              <span class="font-bold">Birth: </span>
+              ${(date_of_birth === "null" || date_of_birth === "undefined" || date_of_birth === "") 
+                  ? "Not available" 
+                  : date_of_birth}
+              </li>
+              <li>
+              <i class="text-xl mr-3 text-brandColor fa-solid fa-mercury"></i> 
+              <span class="font-bold">Gender: </span>
+              ${(gender === "null" || gender === "undefined" || gender === "") 
+                  ? "Not available" 
+                  : gender}
+              </li>
+              <li>
+                <i class="text-xl mr-3 text-brandColor fa-solid fa-solid fa-tags"></i> 
+                <span class="font-bold">Price: </span>${(price === "null" || price === "undefined" || price === "") 
+                  ? "Not available" 
+                  : `${price} $`}
+              </li>
+              <li>
+                <i class="text-xl mr-3 text-brandColor fa-solid fa-solid fa-syringe"></i> 
+                <span class="font-bold">Vaccinated Status: </span>${(vaccinated_status === "null" || vaccinated_status === "undefined" || vaccinated_status === "") 
+                  ? "Not available" 
+                  : vaccinated_status}
+              </li>
+          </ul>
+      </div>
+      <h2 class="text-xl font-bold mb-2">Detail Information:</h2>
+      <p>${(pet_details === "null" || pet_details === "undefined" || pet_details === "") 
+        ? "Not available" 
+        : pet_details}
+      </p>
+      <button onclick="closeModal()" class="py-3 px-8 mt-6 w-full bg-brandColor text-white rounded-xl opacity-80 hover:opacity-100 transition-opacity duration-500">Close</button>
+    </div>
+  `;
+  modal.appendChild(div);
   modal.style.display = 'flex';
 }
-//------Close the modal window-------//
+// Close modal function
 function closeModal() {
   const modal = document.getElementById('confirmationModal');
   modal.style.display = 'none';
 }
 
 
-//-------- Modal for Adopt done ----------//
+
+
+//-------- Modal for Adoption done ----------//
 function showAdoptModal(message) {
   const modal = document.getElementById('countdownModal');
   const countdownElement = document.getElementById('countdown');
